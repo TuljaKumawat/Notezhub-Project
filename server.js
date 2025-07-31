e7db077ba8f00e6a3791cd2666e3cbfceb747618
Tulja Kumawat <tuljakumawat@gmail.com>
2025-07-31 14:54:37 +0530

    Added root route to redirect to /user

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 require('dotenv').config()
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 require('./dbconfiguration/dbconfiguration.js')
 const session = require('express-session')
-const userRouter=require('./routers/userrouter')
-const adminRouter=require('./routers/adminrouter')
+const userRouter = require('./routers/userrouter')
+const adminRouter = require('./routers/adminrouter')
 
 app.use(session({
-    secret:process.env.KEY,
-    resave:false,
-    saveUninitialized:false,
-    cookie:{maxAge:1000*60*24*365}
+    secret: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 24 * 365 }
 }))
 
-
+app.get('/', (req, res) => {
+    res.redirect('/user');
+})
 // app.use('/ad',adminRouter)
 // //app.use(homeRouter)
-app.use('/user',userRouter)
+app.use('/user', userRouter)
 app.use(express.static('public'))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.listen(process.env.PORT,()=>{console.log(`Server is running on port ${process.env.PORT}`)})
+app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`) })
